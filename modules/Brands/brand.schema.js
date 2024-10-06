5e1faec0c40333563c3dfbcff64df06266b3118c
Rajesh3859 <rajeshvjm10@gmail.com>
2024-10-06 19:36:39 +0530

    6-10

--- modules/Brands/brand.schema.js
+++ modules/Brands/brand.schema.js
@@ -19,7 +19,7 @@ const BrandSchema = mongoose.Schema(
       default: false,
     },
     imageUrl: {
-      type: URL,
+      type: String,
       default:
         "https://media.istockphoto.com/id/467939334/photo/golden-wedding-rings.jpg?s=2048x2048&w=is&k=20&c=v-YzoJgBPtQbH5s6QgYg-kEF5fbyNFOkpE2lhdVEqBY=",
     },
